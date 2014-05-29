2e8a1cbe62e18724ba2a462f69804bc505c88b55
nicolo.brognoli@gmail.com <nicolo.brognoli@gmail.com@a712b97e-e513-a204-0ad6-904687842aba>
2014-05-29 22:24:49 +0000

    Gestione immagini

--- TrenitaliaApp/src/com/example/trenitaliaapp/utils/SDCard.java
+++ TrenitaliaApp/src/com/example/trenitaliaapp/utils/SDCard.java
@@ -366,4 +366,68 @@ public class SDCard
         return bitmap;
     }
     
+    public static Bitmap getPreviewImage(User user, int tipo)
+    {
+        File root = android.os.Environment.getExternalStorageDirectory();
+        
+        String fileName = root.getAbsolutePath() + "/" + APPFOLDER + "/" + user.getNumero() + "/";
+        
+        if (tipo == VISO)
+        {
+            fileName = fileName + VISO_FILENAME;
+        }
+        else if (tipo == DOCUMENTO)
+        {
+            fileName = fileName + DOCUMENTO_FILENAME;
+        }
+        
+        Bitmap bitmap;
+        
+        try
+        {
+            
+            
+         // First decode with inJustDecodeBounds=true to check dimensions
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(fileName, options);
+
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, 50, 50);
+
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            bitmap = BitmapFactory.decodeFile(fileName, options);
+        }
+        catch (Exception e)
+        {
+            Log.e(TAG, e.getMessage());
+            return null;
+        }
+        return bitmap;
+    }
+    
+    private static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    // Raw height and width of image
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+
+        final int halfHeight = height / 2;
+        final int halfWidth = width / 2;
+
+        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+        // height and width larger than the requested height and width.
+        while ((halfHeight / inSampleSize) > reqHeight
+                && (halfWidth / inSampleSize) > reqWidth) {
+            inSampleSize *= 2;
+        }
+    }
+
+    return inSampleSize;
+}
+    
 }
